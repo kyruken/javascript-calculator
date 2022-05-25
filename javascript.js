cb024646eb63337bbc7a1c57045ee3420a5df762
Avery Kyle Arenas <kyruken@gmail.com>
2022-05-25 15:12:49 -0700

    fix equal button, now works

--- javascript.js
+++ javascript.js
@@ -1,26 +1,45 @@
 function add(operand1, operand2) {
-    return operand1 + operand2;
+    return Number(operand1) + Number(operand2);
 }
 
 function subtract(operand1, operand2) {
-    return operand1 - operand2;
+    return Number(operand1) - Number(operand2);
 }
 
 function multiply(operand1, operand2) {
-    return operand1 * operand2;
+    return Number(operand1) * Number(operand2);
 }
 
 function divide(operand1, operand2) {
-    return operand1 / operand2;
+    return Number(operand1) / Number(operand2);
 }
 
 function operate(operator, operand1, operand2) {
+    if (operator === "+") {
+        return add(operand1, operand2);
+    }
+
+    if (operator === "-") {
+        return subtract(operand1, operand2);
+    }
+
+    if (operator === "*") {
+        return multiply(operand1, operand2);
+    }
+
+
+    if (operator === "/") {
+        return divide(operand1, operand2);
+    }
+
 
 }
 
 function display() {
     let numberButtons = document.querySelectorAll(".number-btn");
     let operateButtons = document.querySelectorAll(".operation-btn");
+    let equalButton = document.querySelector(".equal-btn");
+
     let display = document.querySelector(".display");
     let inputValue = 0;
     let tempValue = 0;
@@ -35,12 +54,20 @@ function display() {
 
     operateButtons.forEach(button => { button.addEventListener('click', () => {
         let operator = button.textContent;
+        operation = operator;
         tempValue = inputValue;
         console.log(tempValue);
 
 
     });
+
 });
+
+    equalButton.addEventListener('click', () => {
+        display.textContent = operate(operation, tempValue, inputValue);
+        
+
+    });
 
 }
 
